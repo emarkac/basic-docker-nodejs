cd28af3ab771a82a7ea2f727d71c955b5d7ffbe6
emarkac <emarkac@gmail.com>
2022-02-07 13:11:04 +0100

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require("express");
 const app = express();
 
-const VERSION = 1.0.1;
+const VERSION = 2.0.0;
 
 app.get("/", (req, res) => {
   res.send(`Current version: ${VERSION}`);
